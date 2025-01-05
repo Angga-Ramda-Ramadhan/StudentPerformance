f7000ebb8c1f41170f8d84c669a6530eb72ac435
AnggaRamdaRamadhan <anggaramdawork@gmail.com>
2025-01-05 19:01:17 +0700

    Unit test dan Flake8

--- utils/correlation.py
+++ utils/correlation.py
@@ -1,9 +1,9 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+
 def checkcor(data):
     corr = data.corr()
-    plt.figure(figsize=(5,5))
+    plt.figure(figsize=(5, 5))
     sns.heatmap(corr, annot=True)
     plt.title("Korelasi Data Performance Students")
-
--- utils/modelling.py
+++ utils/modelling.py
@@ -2,17 +2,16 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import GridSearchCV
-import pandas as pd
-
 
 
 def splitting(data):
     test_size = 0.2
-    x=data.iloc[:,:-1]
-    y=data.iloc[:,-1]
+    x = data.iloc[:, :-1]
+    y = data.iloc[:, -1]
 
     X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=test_size)
     return X_train, X_test, Y_train, Y_test
+
 
 def model_RF(X_train, X_test, Y_train, Y_test):
     Rf = RandomForestRegressor()
@@ -30,8 +29,8 @@ def model_RF(X_train, X_test, Y_train, Y_test):
 
 def model_RF_HP(X_train, X_test, Y_train, Y_test):
     params = {
-        "n_estimators": [50,100,200],
-        "max_depth":[3,5,10]
+        "n_estimators": [50, 100, 200],
+        "max_depth": [3, 5, 10]
     }
     Rf = RandomForestRegressor()
     grid = GridSearchCV(Rf, params)
@@ -46,5 +45,3 @@ def model_RF_HP(X_train, X_test, Y_train, Y_test):
     print(f"Hasil MSE = {mse}, Hasil MAE = {mae}")
 
     return model, data_uji, mae, mse, params
-
-
